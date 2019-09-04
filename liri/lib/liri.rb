bb5321eb46a47d8e056ccee6a3e44a86be959f19
Rodrigo <rofaccess @gmail.com>
2019-09-03 20:42:16 -0400

    Add empty gem structure

---
+++ liri/lib/liri.rb
@@ -0,0 +1,6 @@
+require "liri/version"
+
+module Liri
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ liri/spec/liri_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Liri do
+  it "has a version number" do
+    expect(Liri::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
