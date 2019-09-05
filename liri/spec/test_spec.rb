1508b58b654f64a5982c0597e3f1e6b2d8ca5ede
Rodrigo <rofaccess @gmail.com>
2019-09-04 23:30:07 -0400

    #5 Improved run test

--- liri/spec/test_spec.rb
+++ liri/spec/test_spec.rb
@@ -1,8 +1,9 @@
 require 'test'
 
 RSpec.describe "Test" do
-  it "return true" do
-    expect(Liri::Test.run('spec/liri_spec.rb')).to eq true
+  context 'run' do
+    it "return true" do
+      expect(Liri::Test.run('spec/liri_spec.rb')).to eq true
+    end
   end
-
 end
