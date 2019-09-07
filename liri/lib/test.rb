0599f38bc23c1166399c6c55aee95513d1c58bab
Rodrigo <rofaccess @gmail.com>
2019-09-06 21:21:49 -0400

    Save test run result in file

--- liri/lib/test.rb
+++ liri/lib/test.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 module Liri
   class Test
     def self.run(path)
-      system("bundle exec rspec #{path}")
+      system("bundle exec rspec #{path} --format progress --out rspec_result.txt --no-color")
     end
   end
 end
--- liri/spec/liri_spec.rb
+++ liri/spec/liri_spec.rb
@@ -1,5 +1,11 @@
 RSpec.describe Liri do
-  it "has a version number" do
-    expect(Liri::VERSION).not_to be nil
+  context 'run' do
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
+
+    it "has a version number" do
+      expect(Liri::VERSION).not_to be nil
+    end
   end
 end
