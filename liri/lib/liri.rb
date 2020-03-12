14898c0568228489917aca5a12f93dbcb5c96531
Rodrigo Fernández <rofaccess@gmail.com>
2020-03-12 19:25:10 -0300

    #13 Added script to build and install gem. Updated gem executable

--- liri/lib/liri.rb
+++ liri/lib/liri.rb
@@ -4,12 +4,12 @@ require 'compressor/compressor'
 module Liri
   class << self
     def run
+      puts "Starting Testing Process"
       Compressor.compress
       #Sender.send
 
       #Runner.run
-
+      puts "Finished Testing Process"
     end
-      Liri.run
   end
 end
