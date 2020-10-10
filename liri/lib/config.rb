e98137c0a4a52f22243e46ba4877d5662e306d42
Rodrigo <rofaccess@gmail.com>
2020-10-09 21:07:43 -0300

    #13 Updated compressed source code file name

--- liri/lib/config.rb
+++ liri/lib/config.rb
@@ -2,9 +2,10 @@ require 'fileutils'
 
 module Config
   SOURCE_CODE_DIR = Dir.pwd
+  SOURCE_CODE_DIR_NAME = SOURCE_CODE_DIR.split('/').last
   CONFIG_DIR_NAME = '.liri'
   CONFIG_DIR = File.join(SOURCE_CODE_DIR, '/', CONFIG_DIR_NAME)
-  COMPRESSED_FILE_NAME = 'source_code.zip'
+  COMPRESSED_FILE_NAME = "#{SOURCE_CODE_DIR_NAME}_source_code.zip"
 
   def self.config_dir
     Dir.mkdir(CONFIG_DIR) unless Dir.exist?(CONFIG_DIR)
