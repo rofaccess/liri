278e4ba976dca630e536261c0bc5782c3e094ae7
Rodrigo <rofaccess@gmail.com>
2022-05-13 18:09:26 -0400

    #48 Se mejora el módulo SourceCode y sus tests

--- spec/common/source_code_spec.rb
+++ spec/common/source_code_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe Liri::Common::SourceCode do
 
   context 'cuando el codigo fuente comprimido ya existe' do
     before(:all) do
-      @source_code = Liri::Common::SourceCode.new(source_code_folder_path, compression_class, unit_test_class)
+      @source_code = Liri::Common::SourceCode.new(dummy_app_folder_path, dummy_app_folder_path, compression_class, unit_test_class)
       @source_code.compress_folder
     end
 
@@ -48,38 +48,26 @@ RSpec.describe Liri::Common::SourceCode do
 
     describe '#decompress_file' do
       it 'descomprime el archivo comprimido' do
-        @source_code.compress_folder
         expect(@source_code.decompress_file).to be true
-      end
-
-      after(:all) do
-        #@source_code.delete_compressed_file
-        #@source_code.delete_decompressed_file_folder_path
+        expect(Dir.exist?(@source_code.decompressed_file_folder_path)).to be true
+        @source_code.delete_decompressed_file_folder_path
       end
     end
 
     describe '#delete_compressed_file' do
       it 'borra el archivo comprimido' do
-        @source_code.compress_folder
-        #expect(@source_code.delete_compressed_file).to be true
+        expect(@source_code.delete_compressed_file).to be true
         expect(File.exist?(@source_code.compressed_file_path)).to be false
+        @source_code.compress_folder
       end
     end
 
     describe '#delete_decompressed_file_folder_path' do
       it 'borra la carpeta' do
         Dir.mkdir(@source_code.decompressed_file_folder_path) unless Dir.exist?(@source_code.decompressed_file_folder_path)
-        #expect(@source_code.delete_decompressed_file_folder_path).to be true
+        expect(@source_code.delete_decompressed_file_folder_path).to be true
         expect(Dir.exist?(@source_code.decompressed_file_folder_path)).to be false
       end
     end
-
-    after(:all) do
-      #@source_code.delete_compressed_file
-    end
-  end
-
-  after(:all) do
-    #Liri.delete_setup_folder
   end
 end
